b07b28454de29b14ae66563fba4cb0602e63c757
actuallyroy <amitkumar93525@gmail.com>
2022-11-19 14:28:13 +0530

    some more changes

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
-let tools = [
-    {
-        name: "Text",
+const editorWindow = document.getElementById("editorWindow");
+const toolBar = document.getElementById("toolBar")
+var tempBox = document.getElementById("tempBox")
+
+let isMouseDown = false
+
+
+const tools = {
+    "Input": {
         Icon: '<i class="bi bi-input-cursor-text"></i>',
-        html: '<input>'
+        html: `<input src="" id style="position: absolute; top: posYpx; left:posXpx; height:hgtpx; width:calc(wdtpx - 8px)">`,
+        cursor: 'url("./assets/cursors/input_cursor.svg") 15 15, auto'
     },
 
-    {
-        name: "Image",
+    "Image": {
         Icon: '<i class="bi bi-image"></i>',
-        html: '<img>'
+        html: `<img src="" id style="position: absolute; top: posYpx; left:posXpx; height:hgtpx; width:wdtpx">`,
+        cursor: 'url("./assets/cursors/image_cursor.svg") 15 15, auto'
+    },
+    "Button": {
+        Icon: '<i class="bi bi-dash-square"></i>',
+        html: `<button id style="position: absolute; top: posYpx; left: posXpx; height:hgtpx; width:wdtpx">Button</button>`,
+        cursor: 'url("./assets/cursors/button_cursor.svg") 15 15, auto'
+    },
+    "Multi_Line_Text": {
+        Icon: '<i class="bi bi-textarea-t"></i>',
+        html: `<textarea id style="position: absolute; top: posYpx; left:posXpx; height:hgtpx; width:wdtpx"></textarea>`,
+        cursor: 'crosshair'
+    },
+    "TextBox": {
+        Icon: '<i class="bi bi-fonts"></i>',
+        html: `<p type="text" id style="margin: 0; padding: 0; text-align: center; vertical-align: middle; position: absolute; top: posYpx; left:posXpx; height:hgtpx; width:wdtpx">Text</p>`,
+        cursor: 'crosshair'
     }
-]
+}
 
 
-let toolBar = document.getElementById("toolBar")
+let elementIDs = {
+    "Input": [],
+    "Image": [],
+    "Button": [],
+    "Multi_Line_Text": [],
+    "TextBox": []
+}
 
-tools.forEach(tool => {
-    toolBar.innerHTML += `<button title=${tool.name} class="toolBtn">${tool.Icon}</button>`
+
+Object.keys(tools).forEach(tool => {
+    toolBar.innerHTML += `<button title=${tool} class="toolBtn">${tools[tool].Icon}</button>`
 })
 
-currentSelectedTool = null
+let currentSelectedTool = null
 
 let toolBtns = document.querySelectorAll(".toolBtn");
 
 toolBtns.forEach(item => {
     item.addEventListener("click", () => {
-        document.getElementById("editorWindow").style.cursor = "text"
-        console.log(item.title)
+        document.getElementById("editorWindow").style.cursor = tools[item.title].cursor;
         currentSelectedTool = item.title
     })
 })
@@ -34,29 +62,78 @@ toolBtns.forEach(item => {
 let toolX = 0;
 let toolY = 0;
 
-const editorWindow = document.getElementById("editorWindow");
 
-editorWindow.onmousemove = (e) => {
-    toolX = e.clientX;
-    toolY = e.clientY;
-}
-
-editorWindow.onclick = (e) => {
-    if (currentSelectedTool == "Text") {
-        console.log("Text")
-        let input = document.createElement("input");
-        input.style.position = "absolute";
-        input.style.left = toolX + "px";
-        input.style.top = toolY + "px";
-        editorWindow.appendChild(input);
-    }else if(currentSelectedTool = "Image"){
-        let input = document.createElement("img");
-        input.style.position = "absolute";
-        input.style.left = toolX + "px";
-        input.style.top = toolY + "px";
-        input.height = 100
-        input.width = 100
-        editorWindow.appendChild(input);
+editorWindow.onmousedown = (e) => {
+    if(currentSelectedTool){
+        tempBox = document.getElementById("tempBox")
+        toolX = e.clientX;
+        toolY = e.clientY;
+        tempBox.style.top = toolY + "px"
+        tempBox.style.left = toolX + "px"
+        tempBox.style.height = "0px"
+        tempBox.style.width = "0px"
+        tempBox.style.display = "block"
+        isMouseDown = true
     }
 }
 
+let elemBorder = null
+let elem = null
+let elemHoverChanged = true
+editorWindow.onmousemove = (e) => {
+    if (isMouseDown) {
+        diffX = e.clientX - toolX
+        diffY = e.clientY - toolY
+        tempBox.style.height = diffY + "px"
+        tempBox.style.width = diffX + "px"
+    }else{
+        if(e.target.id != "editorWindow"){
+            if(elem && elem.id != e.target.id){
+                elemHoverChanged = true
+            }
+            elem = document.getElementById(e.target.id)
+            if(elemHoverChanged){
+                elemBorder = elem.style.border
+                elemHoverChanged = false
+            }
+            if(elem){
+                elem.style.border = "2px solid black"
+            }
+            elem.onmouseout = () => {
+                elem.style.border = elemBorder
+            }
+
+            elem.onclick = () => {
+                e.preventDefault();
+            }
+        }
+
+    }
+}
+
+editorWindow.onmouseup = (e) => {
+    if(currentSelectedTool){
+        diffX = e.clientX - toolX
+        diffY = e.clientY - toolY
+        tempBox.style.display = "none"
+        editorWindow.innerHTML += generateElement(currentSelectedTool, toolX, toolY, diffY, diffX)
+        currentSelectedTool = null;
+    }
+    isMouseDown = false;
+    editorWindow.style.cursor = "default"
+}
+
+
+function generateElement(elemName, posX, posY, height, width){
+    if(elemName){
+        let id = elemName.toLowerCase() + elementIDs[elemName].length
+        let htmlTxt = tools[elemName].html.replace(" id ", ` id=${id} `).replace("posX", posX).replace("posY", posY).replace("hgt", height < 20 ? 'auto' : height).replace("wdt", width < 20 ? 'auto' : width)
+        elementIDs[elemName].push(id)
+        return htmlTxt;
+    }
+}
+
+
+editorWindow.onclick = (e) => {
+    e.preventDefault();
+}
